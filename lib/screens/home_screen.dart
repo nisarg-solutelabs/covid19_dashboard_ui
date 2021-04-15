8a0592dca88ed5654fa7b9dfc8c3c31c22a83c50
Nisarg Ratani <nisarg.r@soultelabs.com>
2021-04-15 09:16:24 +0530

    Covid-19 Dashboard UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:covid_19_dashboard_ui/config/palette.dart';
+import 'package:covid_19_dashboard_ui/config/styles.dart';
 import 'package:covid_19_dashboard_ui/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 
@@ -8,50 +9,134 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String _country = 'UK';
   @override
   Widget build(BuildContext context) {
     final screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
       appBar: CustomAppBar(),
       body: CustomScrollView(
-        physics: ClampingScrollPhysics(),
+        physics: const ClampingScrollPhysics(),
         slivers: [
           _buildHeader(screenHeight),
         ],
       ),
     );
   }
-}
 
-SliverToBoxAdapter _buildHeader(double screenHeight) {
-  return SliverToBoxAdapter(
-    child: Container(
-      padding: const EdgeInsets.all(20.0),
-      decoration: BoxDecoration(
-        color: Palette.primaryColor,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(40.0),
-          bottomRight: Radius.circular(40.0),
+  SliverToBoxAdapter _buildHeader(double screenHeight) {
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.all(20.0),
+        decoration: const BoxDecoration(
+          color: Palette.primaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(40.0),
+            bottomRight: Radius.circular(40.0),
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  'COVID-19',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                CountryDropdown(
+                  countries: ['CN', 'FR', 'IN', 'IT', 'UK', 'USA'],
+                  country: _country,
+                  onChanged: (val) {
+                    setState(() {
+                      _country = val;
+                    });
+                  },
+                ),
+              ],
+            ),
+            SizedBox(
+              height: screenHeight * 0.03,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Are you feeling sick?',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22.0,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.01,
+                ),
+                const Text(
+                  'If you feel sick with any COVID-19 symptoms,please call or text us immediately for help',
+                  style: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 15.0,
+                  ),
+                ),
+                SizedBox(
+                  height: screenHeight * 0.03,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    FlatButton.icon(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      color: Colors.red,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.phone,
+                        color: Colors.white,
+                      ),
+                      label: const Text(
+                        'Call Now',
+                        style: Styles.buttonTextStyle,
+                      ),
+                      textColor: Colors.white,
+                    ),
+                    FlatButton.icon(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 10.0,
+                        horizontal: 20.0,
+                      ),
+                      color: Colors.blue,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0),
+                      ),
+                      onPressed: () {},
+                      icon: const Icon(
+                        Icons.chat_bubble,
+                        color: Colors.white,
+                      ),
+                      label: const Text(
+                        'Send SMS',
+                        style: Styles.buttonTextStyle,
+                      ),
+                      textColor: Colors.white,
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Row(
-            children: [
-              Text(
-                'COVID-19',
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 25.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              CountryDropdown(),
-            ],
-          )
-        ],
-      ),
-    ),
-  );
+    );
+  }
 }
--- lib/widgets/country_dropdown.dart
+++ lib/widgets/country_dropdown.dart
@@ -1,16 +1,53 @@
 import 'package:flutter/material.dart';
 
-class CounterDropdown extends StatelessWidget {
+class CountryDropdown extends StatelessWidget {
   final List<String> countries;
   final String country;
   final Function(String) onChanged;
 
-  const CounterDropdown(
+  const CountryDropdown(
       {@required this.countries,
       @required this.country,
       @required this.onChanged});
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+      height: 40.0,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(30.0),
+      ),
+      child: DropdownButtonHideUnderline(
+        child: DropdownButton<String>(
+          value: country,
+          items: countries
+              .map((e) => DropdownMenuItem(
+                    value: e,
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 12.0,
+                          child: Image.asset(
+                              'assets/images/${e.toLowerCase()}_flag.png'),
+                        ),
+                        const SizedBox(
+                          width: 8.0,
+                        ),
+                        Text(
+                          e,
+                          style: const TextStyle(
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ))
+              .toList(),
+          onChanged: onChanged,
+        ),
+      ),
+    );
   }
 }
